a77b4f702475f8b48814b8a3d6100fb302152423
butterfly1of4 <jhouck29@gmail.com>
2020-08-04 12:26:05 -0400

    adding Heroku access

--- index.js
+++ index.js
@@ -10,8 +10,15 @@ const NewReleases = require('./models/NewReleases')
 app.get("/", (req, res) => {
     res.send("This is a GET route");
   });
+  
+//Heroku Access
+  app.set("port", process.env.PORT || 8080);
 
+  app.listen(app.get("port"), () => {
+    console.log(`✅ PORT: ${app.get("port")} 🌟`);
+  });
 
+//Local Access
 app.listen(3000, () => {
     console.log('listening')
 })
